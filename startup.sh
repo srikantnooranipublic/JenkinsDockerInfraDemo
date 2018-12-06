f1a3fe9db431e566fd6845e19cef801146b4f618
Srikant <srikns@yahoo.com>
2018-12-05 22:40:47 -0500

    script changes

--- startup.sh
+++ startup.sh
@@ -3,10 +3,16 @@
 #CONTAINERS=`grep container_name docker-compose.yml |awk  '{print $2}'`
 CONTAINERS=`docker ps -aq`
 for CONTAINER in $CONTAINERS; do
-	echo "removing $CONTAINER
+	echo "removing $CONTAINER"
 	docker stop $CONTAINER
 	docker rm $CONTAINER
 
 done
 
+PWD_NEW=`echo $PWD | sed 's_/_\\\\/_g'`
+sed 's/COMPOSE_DIR/'$PWD_NEW'/g' docker-compose.yml > docker-compose.yml.changed
+/bin/mv -f docker-compose.yml.changed docker-compose.yml
+
 docker-compose up -d
+
+docker ps
