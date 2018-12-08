7fbd8c6cec2f180935d523c8d28a5852def03f51
Srikant <srikns@yahoo.com>
2018-12-08 01:41:39 -0500

    changes

--- app/run.sh
+++ app/run.sh
@@ -1,5 +1,5 @@
 docker run -d \
--p 8888:8080 \
+-p 8999:8080 \
 --add-host=centOS7.5-aop2.dev.fco:10.238.32.21 \
 -v /opt/ca/JenkinsPipelineApp/DockerApp/APP_RUNTIME/logs:/opt/ca/springbootApp/wily/logs \
 --name=apm-agent \
--- jenkins/run.sh
+++ jenkins/run.sh
@@ -1,2 +1,5 @@
-docker run -d -v /opt/ca/jenkins/jenkins_home:/var/jenkins_home --env JAVA_OPTS="-Dhudson.model.DirectoryBrowserSupport.CSP="  --name=jenkins -p 8080:8080 -p 50000:50000 jenkins/jenkins 
+docker run -d -v /var/run/docker.sock:/var/run/docker.sock \
+                -v $(which docker):/usr/bin/docker \
+		-v /Users/noosr03/Documents/CA_Technical/2_ProjectRepo/JenkinsDockerInfra/jenkins/jenkins_home:/var/jenkins_home \
+		--env JAVA_OPTS="-Dhudson.model.DirectoryBrowserSupport.CSP="  --name=jenkins -p 8080:8080 -p 50000:50000 jenkins/jenkins 
 
