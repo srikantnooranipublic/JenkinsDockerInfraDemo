1e318f52fc2a37ae6403b0fe4eb739f76a284871
Srikant <srikns@yahoo.com>
2018-12-10 19:58:03 -0500

    jmeter changes

--- jmeter/JMETER_FILES/entrypoint.sh
+++ jmeter/JMETER_FILES/entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
   
+echo " APP HOST is ${APP_HOST}"
+echo " APP PORT is ${APP_PORT}"
+
 sed -i 's/APP_HOST/'"${APP_HOST}"'/g' ${TEST_HOME}/JenkinsJmeterTest.jmx
 sed -i 's/APP_PORT/'"${APP_PORT}"'/g' ${TEST_HOME}/JenkinsJmeterTest.jmx
 
-${JMETER_HOME}/bin/jmeter  -n -t ${TEST_HOME}/JenkinsJmeterTest.jmx -l ${TEST_HOME}/result.jtl
+${JMETER_HOME}/apache-jmeter-5.0/bin/jmeter  -n -t ${TEST_HOME}/JenkinsJmeterTest.jmx -l ${TEST_HOME}/result.jtl
--- jmeter/run.sh
+++ jmeter/run.sh
@@ -1,1 +1,1 @@
-docker run -d -v /opt/ca/JenkinsPipelineApp/DockerJmeter/JMETER_RUNTIME/:/opt/jmeter  --name=jmeter  --env APP_HOST="10.238.238.40"  --env APP_PORT="8888" jmeter
+docker run -d   --name=jmeter  --env APP_HOST="172.17.0.1"  --env APP_PORT="8888" jmeter
