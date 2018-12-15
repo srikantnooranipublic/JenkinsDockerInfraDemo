9c54329708d61765db70e13f06e546c4a789ae70
Srikant <srikns@yahoo.com>
2018-12-15 11:54:23 -0500

    startup changed

--- startup.sh
+++ startup.sh
@@ -11,7 +11,7 @@ done
 
 PWD_NEW=`echo $PWD | sed 's_/_\\\\/_g'`
 DOCKER_PATH=`echo $(which docker)|sed 's_/_\\\\/_g'`
-sed 's/HOST_MOUNT_DIR/'$PWD_NEW'/g' docker-compose.yml > docker-compose.yml.changed
+sed 's/HOST_MOUNT_DIR/'$PWD_NEW'/g' docker-compose.yml.template > docker-compose.yml.changed
 sed 's/HOST_DOCKER_PATH/'$DOCKER_PATH'/g' docker-compose.yml.changed > docker-compose.yml.changed1
 /bin/mv -f docker-compose.yml.changed1 docker-compose.yml
 /bin/rm -f docker-compose.yml.changed
