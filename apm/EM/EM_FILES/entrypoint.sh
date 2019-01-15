00f91b88363a6985fc7d45e567beae3a0168228a
srikant srikant <srikns@yahoo.com>
2019-01-14 23:37:38 -0500

    EM fixes

--- apm/EM/EM_FILES/entrypoint.sh
+++ apm/EM/EM_FILES/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-/bin/cp -r /opt/CA/Introscope/config/tess-db-cfg.xml.orig /opt/CA/Introscope/config/tess-db-cfg.xml
+/bin/cp -f /opt/CA/Introscope/config/tess-db-cfg.xml.orig /opt/CA/Introscope/config/tess-db-cfg.xml
 
 /opt/CA/Introscope/Introscope_Enterprise_Manager
