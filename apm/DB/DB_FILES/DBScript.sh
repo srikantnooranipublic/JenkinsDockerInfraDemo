df56f31cd9cfee0bf55007fd69e0509792446ad1
Srikant <srikns@yahoo.com>
2019-01-19 18:39:05 -0500

    fixed host and port on db script

--- apm/DB/DB_FILES/DBScript.sh
+++ apm/DB/DB_FILES/DBScript.sh
@@ -1,7 +1,7 @@
 if [ x"$1" == "xbackup" ]; then
-	/usr/local/bin/dbbackup-postgres.sh localhost /usr/local/ cemdb admin CAdemo123# 5432 /CEM_DB cemdbBck.data
+	/usr/local/bin/dbbackup-postgres.sh 172.17.0.1 /usr/local/ cemdb admin CAdemo123# 5444 /CEM_DB cemdbBck.data
 fi
 
 if [ x"$1" == "xrestore" ]; then
-	/usr/local/bin/dbrestore-postgres.sh localhost /usr/local postgres CAdemo123# cemdb admin CAdemo123# 5432 /CEM_DB/cemdbBck.data
+	/usr/local/bin/dbrestore-postgres.sh 172.17.0.1 /usr/local postgres CAdemo123# cemdb admin CAdemo123# 5444 /CEM_DB/cemdbBck.data
 fi
