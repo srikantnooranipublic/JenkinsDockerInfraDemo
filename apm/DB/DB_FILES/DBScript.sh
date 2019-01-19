4756369064a4076558ca3737a79eac1ff6d1df1d
Srikant <srikns@yahoo.com>
2019-01-19 18:14:41 -0500

    changing cemdb folder

--- apm/DB/DB_FILES/DBScript.sh
+++ apm/DB/DB_FILES/DBScript.sh
@@ -1,7 +1,7 @@
 if [ x"$1" == "xbackup" ]; then
-	/usr/local/bin/dbbackup-postgres.sh localhost /usr/local/ cemdb admin CAdemo123# 5432 /var/lib/postgresql/data/ cemdbBck.data
+	/usr/local/bin/dbbackup-postgres.sh localhost /usr/local/ cemdb admin CAdemo123# 5432 /CEM_DB cemdbBck.data
 fi
 
 if [ x"$1" == "xrestore" ]; then
-	/usr/local/bin/dbrestore-postgres.sh localhost /usr/local postgres CAdemo123# cemdb admin CAdemo123# 5432 /var/lib/postgresql/data/cemdbBck.data
+	/usr/local/bin/dbrestore-postgres.sh localhost /usr/local postgres CAdemo123# cemdb admin CAdemo123# 5432 /CEM_DB/cemdbBck.data
 fi
--- startup.sh
+++ startup.sh
@@ -104,7 +104,7 @@ docker-compose up -d
 
 
 LOG ""
-LOG "******* Pls wait while the containers are coiming up"
+LOG "******* Pls wait while the containers are coming up"
 LOG ""
 
 sleep 15
@@ -114,8 +114,6 @@ LOG " Restoring APM DB"
 sleep 3
 
 docker stop apm-em
-
-cp apm/DB/DB_FILES/cemdbBck.data apm/DB/PGDATA/
 
 docker exec -it apm-db sh -c "/usr/local/bin/DBScript.sh restore"
 
