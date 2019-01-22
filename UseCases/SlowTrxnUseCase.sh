b6e9c6bdd5b011be80b16d73bf04980bd81681ad
Srikant <srikns@yahoo.com>
2019-01-22 00:38:26 -0500

    running UC in loop

--- UseCases/SlowTrxnUseCase.sh
+++ UseCases/SlowTrxnUseCase.sh
@@ -1,11 +1,22 @@
+#!/bin/bash
 # Slow Transaction Use Case
 
 echo "Running Slow Trx Use Case"
 echo ""
-curl -X GET \
-  http://localhost:9999/inventory/users \
-  -H 'Postman-Token: cda482c9-e1cf-4a8a-9135-e7a1523f94ed' \
-  -H 'cache-control: no-cache' \
-  -H 'x-apm-bt: t=4F2504E0-4F89-9B0C-0305E82C2301;d=2b6f0cc904d137be2e1730235f5664094b831186;v=1.0;n=ATT;l=3g;g=47.6202268,-122.3490379;a=BROADCOM_MOBILE$bs=BROADCOM_MOBILE;bt=Get Users;p=iOS;pv=8.1'
 
+COUNTER=0
+while [ $COUNTER -lt 12 ]; do
 
+echo "running Slow Transaction $COUNTER of 12 times "
+echo ""
+
+  curl -X GET \
+    http://localhost:8999/inventory/users \
+      -H 'Postman-Token: cda482c9-e1cf-4a8a-9135-e7a1523f94ed' \
+      -H 'cache-control: no-cache' \
+      -H 'x-apm-bt: t=4F2504E0-4F89-9B0C-0305E82C2301;d=2b6f0cc904d137be2e1730235f5664094b831186;v=1.0;n=ATT;l=3g;g=47.6202268,-122.3490379;a=BROADCOM_MOBILE$bs=BROADCOM_MOBILE;bt=Get Users;p=iOS;pv=8.1'
+
+        sleep 2
+
+  let COUNTER=COUNTER+1
+done
