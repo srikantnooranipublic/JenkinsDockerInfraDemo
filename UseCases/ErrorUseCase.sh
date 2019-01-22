44a0b4bee974e9139812e9207a8f53c6650497be
Srikant <srikns@yahoo.com>
2019-01-21 23:16:54 -0500

    tweaks

--- UseCases/ErrorUseCase.sh
+++ UseCases/ErrorUseCase.sh
@@ -1,6 +1,8 @@
 #Error Transaction
+echo "Running Error Use Case"
+echo ""
 curl -X GET \
-  http://10.238.238.40:8888/inventory/printers \
+  http://localhost:9999/inventory/printers \
   -H 'Postman-Token: 1963ff39-b9c5-41fd-8b03-9a2c85b269c4' \
   -H 'cache-control: no-cache' \
   -H 'x-apm-bt: t=4F2504E0-4F89-9B0C-0305E82C2301;d=2b6f0cc904d137be2e1730235f5664094b831186;v=1.0;n=ATT;l=3g;g=47.6202268,-122.3490379;a=BROADCOM_MOBILE$bs=BROADCOM_MOBILE;bt=Get Printers;p=iOS;pv=8.1'
--- UseCases/SlowTrxnUseCase.sh
+++ UseCases/SlowTrxnUseCase.sh
@@ -1,6 +1,9 @@
 # Slow Transaction Use Case
+
+echo "Running Slow Trx Use Case"
+echo ""
 curl -X GET \
-  http://10.238.238.40:8888/inventory/users \
+  http://localhost:9999/inventory/users \
   -H 'Postman-Token: cda482c9-e1cf-4a8a-9135-e7a1523f94ed' \
   -H 'cache-control: no-cache' \
   -H 'x-apm-bt: t=4F2504E0-4F89-9B0C-0305E82C2301;d=2b6f0cc904d137be2e1730235f5664094b831186;v=1.0;n=ATT;l=3g;g=47.6202268,-122.3490379;a=BROADCOM_MOBILE$bs=BROADCOM_MOBILE;bt=Get Users;p=iOS;pv=8.1'
